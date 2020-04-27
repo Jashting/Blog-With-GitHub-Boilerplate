e885f6be9c911e35aafbdde4cf5c47ace1e5e10d
Jashting <sunmmying@gmail.com>
2020-04-27 15:16:46 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,14 +14,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "test_blog"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+author = "Sunmy"
+email = "hello.abc@com"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+description = "战争即和平，自由即奴役，无知即力量。"
+key_words = ['Maverick', 'Sunmy', 'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
